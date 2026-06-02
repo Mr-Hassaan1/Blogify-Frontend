2f66f23c702322091488b7c0cf3e8f66c4fd574a
Mr-Hassaan1 <md.hassaan.tahir@gmail.com>
2026-06-02 11:20:50 +0500

    fixed route issues

--- frontend/src/pages/Blog.jsx
+++ frontend/src/pages/Blog.jsx
@@ -13,7 +13,7 @@ const Blog = () => {
     const getAllPublishedBlogs = async () => {
       try {
         const res = await axios.get(
-          `http://localhost:3200/api/v1/blog//get-published-blogs`,
+          `http://localhost:3200/api/v1/blog/get-published-blogs`,
           { withCredentials: true },
         );
         if (res.data.success) {
